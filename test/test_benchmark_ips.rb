e5d003364f353872c8787b80debd0679ea0fa0a3
Davy Stevenson <davy.stevenson@gmail.com>
2014-05-27 13:40:08 +0100

    Modularize Benchmark::Compare
    Pull compare method into new module Benchmark::Compare. Could be pulled
    out into it's own gem possibly.

--- test/test_benchmark_ips.rb
+++ test/test_benchmark_ips.rb
@@ -14,13 +14,20 @@ class TestBenchmarkIPS < Minitest::Test
 
   def test_ips
     reports = Benchmark.ips(1,1) do |x|
-      x.report("sleep") { sleep(0.25) }
+      x.report("sleep 0.25") { sleep(0.25) }
+      x.report("sleep 0.05") { sleep(0.05) }
+      x.compare!
     end
 
-    rep = reports.first
+    rep1 = reports[0]
+    rep2 = reports[1]
 
-    assert_equal "sleep", rep.label
-    assert_equal 4, rep.iterations
-    assert_in_delta 4.0, rep.ips, 0.2
+    assert_equal "sleep 0.25", rep1.label
+    assert_equal 4, rep1.iterations
+    assert_in_delta 4.0, rep1.ips, 0.2
+
+    assert_equal "sleep 0.05", rep2.label
+    assert_equal 20, rep2.iterations
+    assert_in_delta 20.0, rep2.ips, 0.5
   end
 end
