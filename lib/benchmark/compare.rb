7f8340ec28a4e7c5492e52699b652c8aabeedc70
Juanito Fatas <katehuang0320@gmail.com>
2014-06-19 22:56:10 +0800

    Document Benchmark::Compare module.

--- lib/benchmark/compare.rb
+++ lib/benchmark/compare.rb
@@ -1,6 +1,34 @@
 module Benchmark
+  # Functionality of performaing comparison between reports.
+  #
+  # Usage:
+  #
+  # Add +x.compare!+ to perform comparison between reports.
+  #
+  # Example:
+  #   > Benchmark.ips do |x|
+  #     x.report('Reduce using tag')     { [*1..10].reduce(:+) }
+  #     x.report('Reduce using to_proc') { [*1..10].reduce(&:+) }
+  #     x.compare!
+  #   end
+  #
+  #   Calculating -------------------------------------
+  #       Reduce using tag     19216 i/100ms
+  #   Reduce using to_proc     17437 i/100ms
+  #   -------------------------------------------------
+  #       Reduce using tag   278950.0 (±8.5%) i/s -    1402768 in   5.065112s
+  #   Reduce using to_proc   247295.4 (±8.0%) i/s -    1238027 in   5.037299s
+  #
+  #   Comparison:
+  #       Reduce using tag:   278950.0 i/s
+  #   Reduce using to_proc:   247295.4 i/s - 1.13x slower
+  #
+  # Besides regular Calculating report, this will also indicates which one is slower.
   module Compare
 
+    # Compare between reports, prints out facts of each report:
+    # runtime, comparative speed difference.
+    # @param reports [Array<Report>] Reports to compare.
     def compare(*reports)
       return if reports.size < 2
 
