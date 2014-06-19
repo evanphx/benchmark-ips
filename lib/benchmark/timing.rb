938935dd388dc3f21d6ff74bbc22347179e658e3
Juanito Fatas <katehuang0320@gmail.com>
2014-06-20 01:00:33 +0800

    Document Benchmark::Timing.

--- lib/benchmark/timing.rb
+++ lib/benchmark/timing.rb
@@ -1,10 +1,18 @@
 module Benchmark
+  # Perform caclulations on Timing results.
   module Timing
+
+    # Calculate (arithmetic) mean of given samples.
+    # @param [Array] samples Samples to calculate mean.
+    # @return [Float] Mean of given samples.
     def self.mean(samples)
       sum = samples.inject(0) { |acc, i| acc + i }
       sum / samples.size
     end
 
+    # Calculate variance of given samples.
+    # @param [Float] m Optional mean (Expected value).
+    # @return [Float] Variance of given samples.
     def self.variance(samples, m=nil)
       m ||= mean(samples)
 
@@ -13,10 +21,17 @@ module Benchmark
       total / samples.size
     end
 
+    # Calculate standard deviation of given samples.
+    # @param [Array] samples Samples to calculate standard deviation.
+    # @param [Float] m Optional mean (Expected value).
+    # @return [Float] standard deviation of given samples.
     def self.stddev(samples, m=nil)
       Math.sqrt variance(samples, m)
     end
 
+    # Resample mean of given samples.
+    # @param [Integer] resample_times Resample times, defaults to 100.
+    # @return [Array] Resampled samples.
     def self.resample_mean(samples, resample_times=100)
       resamples = []
 
@@ -28,6 +43,7 @@ module Benchmark
       resamples
     end
 
+    # Recycle unsed objects by starting Garbage Collector.
     def self.clean_env
       # rbx
       if GC.respond_to? :run
