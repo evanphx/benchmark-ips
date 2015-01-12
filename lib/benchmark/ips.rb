75d789bfd76742987dc466c5170301f22a589b75
Evan Phoenix <evan@phx.io>
2015-01-12 09:41:50 -0800

    Bump to 2.1.1

--- lib/benchmark/ips.rb
+++ lib/benchmark/ips.rb
@@ -11,7 +11,7 @@ module Benchmark
   module IPS
 
     # Benchmark-ips Gem version.
-    VERSION = "2.1.0"
+    VERSION = "2.1.1"
 
     # CODENAME of current version.
     CODENAME = "Springtime Hummingbird Dance"
