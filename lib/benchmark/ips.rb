e86b9972c7e55554f9e2765367b7c66ca810282b
Evan Phoenix <evan@phx.io>
2016-08-18 16:51:40 -0700

    Bump to 2.7.2

--- lib/benchmark/ips.rb
+++ lib/benchmark/ips.rb
@@ -15,7 +15,7 @@ module Benchmark
   module IPS
 
     # Benchmark-ips Gem version.
-    VERSION = "2.7.1"
+    VERSION = "2.7.2"
 
     # CODENAME of current version.
     CODENAME = "Cultivating Confidence"
