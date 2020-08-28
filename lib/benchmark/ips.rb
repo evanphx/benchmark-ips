2908207009e2da258f52802cd479b928e4355004
Nate Berkopec <nate.berkopec@gmail.com>
2020-08-28 09:03:06 -0500

    2.8.3

--- lib/benchmark/ips.rb
+++ lib/benchmark/ips.rb
@@ -16,7 +16,7 @@ module Benchmark
   module IPS
 
     # Benchmark-ips Gem version.
-    VERSION = "2.8.2"
+    VERSION = "2.8.3"
 
     # CODENAME of current version.
     CODENAME = "Tardy Turtle"
