ecc3a7d3b50576684073f55ac3f7e8efdd51e204
Nate Berkopec <nate.berkopec@gmail.com>
2021-10-10 15:09:34 -0600

    2.9.2

--- lib/benchmark/ips.rb
+++ lib/benchmark/ips.rb
@@ -18,7 +18,7 @@ module Benchmark
   module IPS
 
     # Benchmark-ips Gem version.
-    VERSION = "2.9.1"
+    VERSION = "2.9.2"
 
     # CODENAME of current version.
     CODENAME = "Sleepy Sasquatch"
